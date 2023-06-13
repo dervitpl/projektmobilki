2ce0479c03af44e3b3da42b6e0c837bbca6058d1
Pawel Socha <pvwxlsocha@gmail.com>
2023-06-13 10:49:57 +0200

    dark mode

--- Wordle/App.js
+++ Wordle/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, Button, Alert, Switch } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from "expo-status-bar";
 import SplashScreen from './SplashScreen';
@@ -8,6 +8,8 @@ import { colors, CLEAR, ENTER, colorsToEmoji } from './src/constants';
 import * as Clipboard from "expo-clipboard";
 
 const NUMBER_OF_TRIES = 6;
+
+
 
 const copyArray = (arr) => {
   return [...arr.map((rows) => [...rows])];
@@ -18,9 +20,10 @@ const getDayOfMonth = () => {
   return now.getDate();
 };
 
+
 export default function App() {
   const dayOfMonth = getDayOfMonth();
-
+  const [darkMode, setDarkMode] = useState(false);
   const words = [
     "cloud",
     "debug",
@@ -63,15 +66,19 @@ export default function App() {
   const word = words[dayOfMonth - 1];
   const letters = word ? word.split('') : [];
   
-
+  
   const [showSplash, setShowSplash] = useState(true);
   const [showAboutModal, setShowAboutModal] = useState(false);
   const [rows, setRows] = useState(
     new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(""))
   );
+
+  
   const [curRow, setCurRow] = useState(0);
   const [curCol, setCurCol] = useState(0);
   const [gameState, setGameState] = useState("playing");
+  
+  
 
   useEffect(() => {
     if (curRow > 0) {
@@ -178,25 +185,134 @@ export default function App() {
     }, 3000);
   }, []);
 
- 
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
+
 
   const handleAboutPress = () => {
     setShowAboutModal(true);
   };
 
   const handleSettingsPress = () => {
-    // Obsługa naciśnięcia przycisku "settings"
-    console.log("Pressed Settings");
+    setShowSettingsModal(true);
   };
 
   const handleCloseModal = () => {
     setShowAboutModal(false);
+    setShowSettingsModal(false);
   };
   
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const ToggleSwitch = ({ value, onToggle }) => {
+    return (
+      <TouchableOpacity
+        style={[styles.switchContainer, { backgroundColor: value ? 'green' : 'red' }]}
+        onPress={onToggle}
+      >
+        <Text style={styles.switchText}>{value ? 'ON' : 'OFF'}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+
+
   const greenCaps = getAllLettersWithColor(colors.primary);
   const yellowCaps = getAllLettersWithColor(colors.secondary);
   const greyCaps = getAllLettersWithColor(colors.darkgrey);
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: darkMode ? colors.black : colors.white,
+    },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingHorizontal: 16,
+      paddingVertical: 12,
+      backgroundColor: darkMode ? colors.darkgrey : colors.lightgrey,
+    },
+    title: {
+      color: darkMode ? colors.lightgrey : colors.darkgrey,
+      fontSize: 32,
+      fontWeight: "bold",
+      letterSpacing: 7,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+    },
+    button: {
+      backgroundColor: colors.primary,
+      paddingVertical: 8,
+      paddingHorizontal: 12,
+      borderRadius: 5,
+      marginLeft: 8,
+    },
+    buttonText: {
+      color: colors.lightgrey,
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+    map: {
+      alignSelf: "stretch",
+      marginVertical: 20,
+      backgroundColor: darkMode ? colors.black : colors.white,
+    },
+    row: {
+      alignSelf: "stretch",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+    cell: {
+      borderWidth: 3,
+      borderColor: darkMode ? colors.lightgrey : colors.darkgrey,
+      flex: 1,
+      maxWidth: 70,
+      aspectRatio: 1,
+      margin: 3,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: darkMode ? colors.black : colors.white
+    },
+    cellText: {
+      color: darkMode ? colors.lightgrey : colors.darkgrey,
+      fontWeight: 'bold',
+      fontSize: 28,
+    },
+    modalContainer: {
+      flex: 1,
+      backgroundColor: darkMode ? colors.black : colors.white,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    modalTitle: {
+      color: darkMode ? colors.lightgrey : colors.darkgrey,
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 16,
+    },
+    modalText: {
+      color: darkMode ? colors.lightgrey : colors.darkgrey,
+      fontSize: 16,
+      marginHorizontal: 20,
+      marginBottom: 24,
+      textAlign: 'center',
+    },
+    switchContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 10,
+    },
+    switchText: {
+      color: darkMode ? colors.lightgrey : colors.darkgrey,
+      fontSize: 16,
+      marginRight: 10,
+    },
+  });
 
 
   return (
@@ -258,94 +374,33 @@ export default function App() {
               Paweł Socha,
               Kacper Mucha,
               Robert Sternal 
-              dzisiejsze slowo to : {word} ( tylko dla developerow ;) )
+              dzisiejsze slowo to : {word} ( tylko dla developerow ;) 
               </Text>
               <Button title="Close" onPress={handleCloseModal} />
             </View>
           </Modal>
+
+          <Modal visible={showSettingsModal} animationType="slide">
+  <View style={styles.modalContainer}>
+    <Text style={styles.modalTitle}>Settings</Text>
+      <View style={styles.switchContainer}>
+  <Text style={styles.switchText}>Dark Mode</Text>
+  <ToggleSwitch value={darkMode} onToggle={toggleDarkMode} />
+  <Switch
+           trackColor={{ false: '#767577', true: '#81b0ff' }}
+           thumbColor={darkMode ? '#f5dd4b' : '#f4f3f4'}
+           onValueChange={toggleDarkMode}
+           value={darkMode}
+          />
+</View>
+    <Button title="Close" onPress={handleCloseModal} />
+  </View>
+</Modal>
         </>
       )}
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.black,
-    alignItems: "center",
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-  },
-  title: {
-    color: colors.lightgrey,
-    fontSize: 32,
-    fontWeight: "bold",
-    letterSpacing: 7,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
-  button: {
-    backgroundColor: colors.primary,
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-    borderRadius: 5,
-    marginLeft: 8,
-  },
-  buttonText: {
-    color: colors.lightgrey,
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  map: {
-    alignSelf: "stretch",
-    marginVertical: 20,
-  },
-  row: {
-    alignSelf: "stretch",
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  cell: {
-    borderWidth: 3,
-    borderColor: colors.darkgrey,
-    flex: 1,
-    maxWidth: 70,
-    aspectRatio: 1,
-    margin: 3,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cellText: {
-    color: colors.lightgrey,
-    fontWeight: 'bold',
-    fontSize: 28,
-  },
-  modalContainer: {
-    flex: 1,
-    backgroundColor: colors.black,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalTitle: {
-    color: colors.lightgrey,
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  modalText: {
-    color: colors.lightgrey,
-    fontSize: 16,
-    marginHorizontal: 20,
-    marginBottom: 24,
-    textAlign: 'center',
-  },
-});
 
 
